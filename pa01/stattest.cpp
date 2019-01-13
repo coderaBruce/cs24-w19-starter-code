92e19fa6bcd50a82d391196e9c6d7d4c61926f3d
Diba Mirza <dimirza@cs.ucsb.edu>
2019-01-13 10:20:26 -0800

    pa01

---
+++ pa01/stattest.cpp
@@ -0,0 +1,19 @@
+// FILE: stattest.cpp
+//Write your test code in this file
+
+
+#include <cctype>    // Provides toupper
+#include <iostream>  // Provides cout, cin
+#include <cstdlib>   // Provides EXIT_SUCCESS
+#include "stats.h"
+
+
+int main( )
+{
+
+
+    return 0;
+
+}
+
+
